8bb4ba1fc2f02a2a582726d492343dccbcdd81ec
Sufyu <Sufyu14@gmail.com>
2026-07-28 11:32:26 -0500

    answer ready

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.models import QueryRequest, QueryResponse
 # from app.retrieval import retrieve
 from app.retrieval_lambda import retrieve
+from app.retrieval_lambda import answer
 from mangum import Mangum
 
 app = FastAPI()
@@ -13,10 +14,11 @@ app = FastAPI()
 
 @app.post("/query", response_model=QueryResponse)
 async def handle_query(request: QueryRequest):
+    chunk_resp = retrieve(request.question, request.top_k)
     return QueryResponse(
         question=request.question,
-        answer=f"Received your question: {request.question}",
-        chunks=retrieve(request.question, request.top_k),
+        answer=answer(request.question, chunk_resp),
+        chunks=chunk_resp,
         latency_ms=0.0
     )
 
